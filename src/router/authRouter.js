966866ec3610659c8c32bd49ea23a6ab4a3f1592
cherrie235 <mathias.cherrier@gmail.com>
2025-12-11 14:44:10 +0100

    fix: models -> model folder

--- src/router/authRouter.js
+++ src/router/authRouter.js
@@ -1,7 +1,7 @@
 import {Router} from 'express'
 import {login, register, recover} from '../controllers/authController.js'
 import { validateBody } from '../middleware/validation.js';
-import { loginSchema, registerSchema} from '../models/auth.js';
+import { loginSchema, registerSchema} from '../model/auth.js';
 import { authenticateToken } from '../middleware/authenticateToken.js';
 
 const router = Router();
--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -2,7 +2,7 @@ import {Router} from 'express'
 import { adminAuthenticateToken } from '../middleware/authenticateToken.js';
 import { getAllUsers, getUserById } from '../controllers/userController.js';
 import { validateParam } from '../middleware/validation.js';
-import { userIdSchema } from '../models/user.js';
+import { userIdSchema } from '../model/user.js';
 
 const router = Router();
 router.use(adminAuthenticateToken)
