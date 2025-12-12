0184a936cf670f7c7262c285a02cb66b48515ae6
Jonas Le Duff <jonas.leduff@etu.unicaen.fr>
2025-12-12 08:32:26 +0100

    feat : added returns to handlers and fixed 404 errors

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,85 +1,105 @@
-import { response, request } from "express"
-import { usersTable } from "../db/schema.js"
-import bcrypt from 'bcrypt'
-import {db} from '../db/database.js'
-import jwt from "jsonwebtoken"
-import 'dotenv/config'
-import { eq } from "drizzle-orm"
+import { response, request } from "express";
+import { usersTable } from "../db/schema.js";
+import bcrypt from "bcrypt";
+import { db } from "../db/database.js";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+import { eq } from "drizzle-orm";
 
 /**
- * 
- * @param {request} req 
- * @param {response} res 
+ *
+ * @param {request} req
+ * @param {response} res
  */
-export const register = async (req, res) =>{
-    try{
-        const {email, first_name, last_name, password} = req.body
-        const hashedPassword = await bcrypt.hash(password, 12)
-        const [result] = await db.insert(usersTable).values({
-            email,
-            first_name,
-            last_name,
-            "password": hashedPassword
-        }).returning({
-            id: usersTable.id,
-            email: usersTable.email,
-            first_name: usersTable.first_name,
-            last_name: usersTable.last_name
-        })
-        const token = jwt.sign({
-            userId: result.id,
-        },process.env.JWT_SECRET,
-        { expiresIn: '24h'})
-        res.status(201).json({
-            message: "User Created",
-            user: result,
-            token: token
-        })
+export const register = async (req, res) => {
+  try {
+    const { email, first_name, last_name, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const [result] = await db
+      .insert(usersTable)
+      .values({
+        email,
+        first_name,
+        last_name,
+        password: hashedPassword,
+      })
+      .returning({
+        id: usersTable.id,
+        email: usersTable.email,
+        first_name: usersTable.first_name,
+        last_name: usersTable.last_name,
+      });
+    const token = jwt.sign(
+      {
+        userId: result.id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "24h" }
+    );
+    return res.status(201).json({
+      message: "User Created",
+      user: result,
+      token: token,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      error: "Failed to register",
+      errorMessage: error,
+    });
+  }
+};
 
-    }catch(error){
-        console.error(error)
-        res.status(500).json({
-            error:"Failed to register",
-            errorMessage: error
-        })
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const [user] = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.email, email));
+    if (!user) {
+      return res.status(401).json({ error: "invalid email or password" });
     }
-}
+    const isValidPassword = await bcrypt.compare(password, user.password);
 
-export const login = async(req, res)=>{
-    try{
-        const {email, password} = req.body
-        const [user] = await db.select().from(usersTable).where(eq(usersTable.email, email))
-        if (!user){
-            return res.status(401).json({error: "invalid email or password"})
-        }
-        const isValidPassword = await bcrypt.compare(password, user.password)
-
-        if (!isValidPassword){
-            return res.status(401).json({error: "invalid email or password"})
-        }
-        const token = jwt.sign({userId: user.id}, process.env.JWT_SECRET, {expiresIn: '24h'})
-        res.status(200).json({
-            message: 'User logged in successfully',
-            userData:{ 
-                id: user.id,
-                email: user.email,
-                first_name: user.first_name,
-                last_name: user.last_name
-            },
-            token
-        })
-    }catch(error){
-        console.error(error)
-        res.status(500).json({error: "Login failed"})
+    if (!isValidPassword) {
+      return res.status(401).json({ error: "invalid email or password" });
     }
-}
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+    return res.status(200).json({
+      message: "User logged in successfully",
+      userData: {
+        id: user.id,
+        email: user.email,
+        first_name: user.first_name,
+        last_name: user.last_name,
+      },
+      token,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Login failed" });
+  }
+};
 
-export const recover = async(req, res)=>{
-    try{
-        const [user] = await db.select().from(usersTable).where(eq(usersTable.id, req.userId.userId))
+export const recover = async (req, res) => {
+  try {
+    const [user] = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.id, req.userId.userId));
 
-        res.status(200).json({first_name:user.first_name, last_name: user.last_name, email: user.email})
-    }catch(error){
-        res.status(500).json({user: "Failed to recover infos"})
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
     }
-}
+    return res.status(200).json({
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+    });
+  } catch (error) {
+    return res.status(500).json({ user: "Failed to recover infos" });
+  }
+};
--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,44 +1,49 @@
-import { desc, eq } from "drizzle-orm"
-import { db } from "../db/database.js"
-import { usersTable } from "../db/schema.js"
+import { desc, eq } from "drizzle-orm";
+import { db } from "../db/database.js";
+import { usersTable } from "../db/schema.js";
 
-export const getAllUsers = async (req, res) =>{
-    try{
-        const users = await db.select({
-            id: usersTable.id,
-            first_name: usersTable.first_name,
-            last_name: usersTable.last_name,
-            email: usersTable.email,
-            role:usersTable.role,
-            creation_date: usersTable.creation_date
-        }).from(usersTable).orderBy(desc(usersTable.creation_date))
-        res.status(200).json({users:users})
-    }catch(error){
-        console.error(error)
-        res.status(500).json({error: "Failed to retrieve users"})
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await db
+      .select({
+        id: usersTable.id,
+        first_name: usersTable.first_name,
+        last_name: usersTable.last_name,
+        email: usersTable.email,
+        role: usersTable.role,
+        creation_date: usersTable.creation_date,
+      })
+      .from(usersTable)
+      .orderBy(desc(usersTable.creation_date));
+    return res.status(200).json({ users: users });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Failed to retrieve users" });
+  }
+};
 
+export const getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [user] = await db
+      .select({
+        id: usersTable.id,
+        first_name: usersTable.first_name,
+        last_name: usersTable.last_name,
+        email: usersTable.email,
+        role: usersTable.role,
+        creation_date: usersTable.creation_date,
+      })
+      .from(usersTable)
+      .where(eq(usersTable.id, id));
+    if (!user) {
+      return res.status(404).json({ error: `no user with id ${id}` });
     }
-}
-
-export const getUserById = async (req, res) =>{
-    try{
-        const {id} = req.params
-        const [user] = await db.select({
-            id: usersTable.id,
-            first_name: usersTable.first_name,
-            last_name: usersTable.last_name,
-            email: usersTable.email,
-            role:usersTable.role,
-            creation_date: usersTable.creation_date
-        }).from(usersTable).where(eq(usersTable.id, id))
-        if (!user){
-            return res.status(400).json({error:`no user with id ${id}`})
-        }
-        res.status(200).json({user:user})
-    }catch(error){
-        console.error(error)
-        res.status(500).json({error: 'Failed to retrieve user'})
-    }
-}
+    return res.status(200).json({ user: user });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Failed to retrieve user" });
+  }
+};
 
 //todo supr utilisateur a faire et documenter ce que l'on fait de ses collections
