801a03f43981ed602fc3020c804e3de4984f1a53
Jonas Le Duff <jonas.leduff@etu.unicaen.fr>
2025-12-17 09:40:44 +0100

    fix : get flashcards

--- src/controller/flashCardController.js
+++ src/controller/flashCardController.js
@@ -2,39 +2,70 @@ import { eq } from "drizzle-orm";
 import { db } from "../db/database.js";
 import { cardTable, collectionsTable, usersTable } from "../db/schema.js";
 
-export const createFlashCard = async (req, res)=>{
-    try{
-    const { front, back, front_URL, back_URL, collection_id } = req.body;
-    const [result] = await db.select().from(collectionsTable).where(eq(collectionsTable.id, collection_id))
-    if (req.userId.userId != result.owner_id)
-        return res.status(403).json({error: `Collection ${collection_id} is not yours` })
-    const [card] = await db.insert(cardTable).values({front, back, front_URL, back_URL, collection_id}).returning()
-    return res.status(200).json({message: `FlashCard ${card.id} added successfully to collection ${collection_id}`, id: card.id, collection_id: collection_id})
-    }catch(error){
-        console.error(error)
-        return res.status(500).json({error: "internal server error while creating the flashCard"})
+export const createFlashCard = async (req, res) => {
+    try {
+        const { front, back, front_URL, back_URL, collection_id } = req.body;
+        const [result] = await db
+            .select()
+            .from(collectionsTable)
+            .where(eq(collectionsTable.id, collection_id));
+        if (req.userId.userId != result.owner_id)
+            return res
+                .status(403)
+                .json({ error: `Collection ${collection_id} is not yours` });
+        const [card] = await db
+            .insert(cardTable)
+            .values({ front, back, front_URL, back_URL, collection_id })
+            .returning();
+        return res.status(200).json({
+            message: `FlashCard ${card.id} added successfully to collection ${collection_id}`,
+            id: card.id,
+            collection_id: collection_id,
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            error: "internal server error while creating the flashCard",
+        });
     }
-}
-export const getFlashCard = async (req, res)=>{
-    try{
-        const {id} = req.params
-        const [card] = await db.select().from(cardTable).where(eq(cardTable.id, id))
-        const [collection] = await db.select().from(collectionsTable).where(collectionsTable.id = card.collection_id)
-        const [user] = await db.select().from(usersTable).where(eq(usersTable.id, req.userId.userId))
-        if (collection.owner_id == req.userId.userId || collection.visibility == 'PUBLIC' || user.role == 'ADMIN')
-            return res.status(200).json({flashCard: card})
-        return res.status(403).json({error: `you do not have access to the flashCard ${card.id}`})
+};
+export const getFlashCard = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const [card] = await db
+            .select()
+            .from(cardTable)
+            .where(eq(cardTable.id, id));
 
-    }catch(error){
-        console.error(error)
-        return res.status(500).json({error: "internal server error while retrieving the flashCard"})
+        if (!card) {
+            return res
+                .status(404)
+                .json({ error: `FlashCard with id ${id} not found` });
+        }
+        const [collection] = await db
+            .select()
+            .from(collectionsTable)
+            .where(eq(collectionsTable.id, card.collection_id));
+        const [user] = await db
+            .select()
+            .from(usersTable)
+            .where(eq(usersTable.id, req.userId.userId));
+        if (
+            collection.owner_id == req.userId.userId ||
+            collection.visibility == "PUBLIC" ||
+            user.role == "ADMIN"
+        )
+            return res.status(200).json({ flashCard: card });
+        return res.status(403).json({
+            error: `you do not have access to the flashCard ${card.id}`,
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            error: "internal server error while retrieving the flashCard",
+        });
     }
-    
-}
+};
 
-export const editFlashCard = (req, res)=>{
-
-}
-export const deleteFlashCard = (req, res)=>{
-    
-}
+export const editFlashCard = (req, res) => {};
+export const deleteFlashCard = (req, res) => {};
--- src/model/flashCard.js
+++ src/model/flashCard.js
@@ -1,13 +1,13 @@
-import {z} from 'zod'
+import { z } from "zod";
 
 export const createFlashCardSchema = z.object({
     front: z.string().min(1).max(1000),
     back: z.string().min(1).max(1000),
     front_URL: z.string().min(1).max(1000).optional(),
     back_URL: z.string().min(1).max(1000).optional(),
-    collection_id: z.uuid()
-})
+    collection_id: z.uuid(),
+});
 
 export const getFlashCardSchema = z.object({
-    id: z.uuid
-})
+    id: z.uuid(),
+});
