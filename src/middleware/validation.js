8ffc8b38e054b642b76c396a56f710f944163562
Jonas Le Duff <jonas.leduff@etu.unicaen.fr>
2025-12-17 09:39:49 +0100

    feat : logger in validation

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -1,47 +1,47 @@
-import { ZodError, ZodType } from 'zod'
+import { ZodError, ZodType } from "zod";
 
 export const validateBody = (schema) => {
-    return (req, res, next) => {
-        if (schema instanceof ZodType) {
-            try {
-                req.body = schema.parse(req.body)
-                next()
-            } catch (e) {
-                if (e instanceof ZodError) {
-                    return res.status(400).send({
-                        error: 'Validation failed',
-                        details: e.issues,
-                    })
-                }
-
-                return res.status(500).send({
-                    error: 'Internal server Error'
-                })
-
-            }
+  return (req, res, next) => {
+    if (schema instanceof ZodType) {
+      try {
+        req.body = schema.parse(req.body);
+        next();
+      } catch (e) {
+        if (e instanceof ZodError) {
+          return res.status(400).send({
+            error: "Validation failed",
+            details: e.issues,
+          });
         }
+
+        console.error(e);
+        return res.status(500).send({
+          error: "Internal server Error when validating body",
+        });
+      }
     }
-}
+  };
+};
 
 export const validateParam = (schema) => {
-    return (req, res, next) => {
-        if (schema instanceof ZodType) {
-            try {
-                schema.parse(req.params)
-                next()
-            } catch (e) {
-                if (e instanceof ZodError) {
-                    return res.status(400).send({
-                        error: 'Invalid params',
-                        details: e.issues,
-                    })
-                }
-
-                return res.status(500).send({
-                    error: 'Internal server Error'
-                })
-
-            }
+  return (req, res, next) => {
+    if (schema instanceof ZodType) {
+      try {
+        schema.parse(req.params);
+        next();
+      } catch (e) {
+        if (e instanceof ZodError) {
+          return res.status(400).send({
+            error: "Invalid params",
+            details: e.issues,
+          });
         }
+
+        console.error(e);
+        return res.status(500).send({
+          error: "Internal server Error when validating params",
+        });
+      }
     }
-}
+  };
+};
